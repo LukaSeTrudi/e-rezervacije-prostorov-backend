184efb6de16c35f92fa7aff98fbd5c2197a45b17
Luka Pavčnik <luka.pavcnik5@gmail.com>
2022-11-21 19:41:06 +0100

    upd

--- rezervacije/settings.py
+++ rezervacije/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-$pn=pqjoc2(=d&s$ui=qaj=knmp)%nz#cqq83#zu(tq=qf5u8m
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
