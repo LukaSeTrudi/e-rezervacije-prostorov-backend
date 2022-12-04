9393c539421d9c6e63f98482bc2655a0fe996a3b
Luka Pavčnik <luka.pavcnik5@gmail.com>
2022-12-04 20:34:32 +0100

    Added userprofile in admin

--- apps/profiles/admin.py
+++ apps/profiles/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from apps.profiles.models import UserProfile
 # Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'full_name', 'is_company', 'is_active']
+
+admin.site.register(UserProfile, UserProfileAdmin)
