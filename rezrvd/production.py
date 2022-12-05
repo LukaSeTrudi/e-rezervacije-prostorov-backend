d1bcd61df0a5ff478f059521de82c7842b2b6042
Luka Pavčnik <luka.pavcnik5@gmail.com>
2022-12-05 15:25:32 +0100

    t

--- rezrvd/production.py
+++ rezrvd/production.py
@@ -23,7 +23,7 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # DBHOST is only the server name, not the full URL
-hostname = os.environ['DBHOST']
+#hostname = os.environ['DBHOST']
 
 DEBUG = True
 
