65187d3b58c3897bca087f17c70ca887e83ac240
Luka Pavčnik <luka.pavcnik5@gmail.com>
2023-01-03 23:43:25 +0100

    added serializers

--- api/management/analytics/serializers.py
+++ api/management/analytics/serializers.py
@@ -1,7 +1,27 @@
+from apps.locations.models import Location, LocationCourt
+from apps.profiles.models import UserProfile
 from rest_framework import serializers
 from apps.logs.models import Log
 
-class LogListSerializer(serializers.ModelSerializer):    
+class LocationSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Location
+        fields = ('id', 'name')
+class UserProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = UserProfile
+        fields = ('id', 'full_name')
+class CourtSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = LocationCourt
+        fields = ('id', 'name')
+
+
+class LogListSerializer(serializers.ModelSerializer):
+    location = LocationSerializer()
+    court = CourtSerializer()
+    user_profile = UserProfileSerializer()
+
     class Meta:
         model = Log
         fields = ('id', 'user', 'log_type', 'created_at', 'location', 'court', 'user_profile')
